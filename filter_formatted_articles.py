7d1392396550e233e582f642660745120447cf5d
juicylime <amanahlwat@gmail.com>
2023-11-03 15:29:30 -0700

    testing with torch xla

--- filter_formatted_articles.py
+++ filter_formatted_articles.py
@@ -5,38 +5,39 @@ import re
 import logging
 from collections import defaultdict
 from nltk.tokenize import sent_tokenize, word_tokenize
-from transformers import TFAutoModelForTokenClassification, AutoTokenizer, pipeline
-import tensorflow as tf
+from transformers import BertForTokenClassification, AutoTokenizer, pipeline
+import torch
+import torch_xla.core.xla_model as xm
 
 # Ensure NLTK data is downloaded (used for sentence tokenization)
 import nltk
 nltk.download('punkt')
 
+tpu_device = xm.xla_device()
 # Detect hardware, return appropriate distribution strategy
-try:
-    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection
-    # print('Running on TPU ', tpu.cluster_spec().as_dict()['worker'])
-except ValueError:
-    tpu = None
+# try:
+#     tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection
+#     # print('Running on TPU ', tpu.cluster_spec().as_dict()['worker'])
+# except ValueError:
+#     tpu = None
 
-if tpu:
-    tf.config.experimental_connect_to_cluster(tpu)
-    tf.tpu.experimental.initialize_tpu_system(tpu)
-    strategy = tf.distribute.TPUStrategy(tpu)
-else:
-    strategy = tf.distribute.get_strategy()
+# if tpu:
+#     tf.config.experimental_connect_to_cluster(tpu)
+#     tf.tpu.experimental.initialize_tpu_system(tpu)
+#     strategy = tf.distribute.TPUStrategy(tpu)
+# else:
+#     strategy = tf.distribute.get_strategy()
 
-print("REPLICAS: ", strategy.num_replicas_in_sync)
+# print("REPLICAS: ", strategy.num_replicas_in_sync)
 
-with strategy.scope():
-    model = TFAutoModelForTokenClassification.from_pretrained(
-        "dbmdz/bert-large-cased-finetuned-conll03-english")
-    tokenizer = AutoTokenizer.from_pretrained(
-        "dbmdz/bert-large-cased-finetuned-conll03-english")
+# with strategy.scope():
+tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-large-cased-finetuned-conll03-english")
+model = BertForTokenClassification.from_pretrained("dbmdz/bert-large-cased-finetuned-conll03-english")
+model = model.to(tpu_device)
 
-    # Create the NER pipeline
-    ner_pipeline = pipeline(
-        "ner", model=model, tokenizer=tokenizer, grouped_entities=True, device=0)
+# Create the NER pipeline
+ner_pipeline = pipeline(
+    "ner", model=model, tokenizer=tokenizer, grouped_entities=True, device=0)
 
 
 def setup_logging():
@@ -58,43 +59,44 @@ def process_articles(stock_symbol, formatted_articles, entity_names):
 
     for date, info in formatted_articles.items():
         start_time = time.time()  # Store the start time
-        with strategy.scope():
-            for article in info['articles']:
-                # Remove the [ENT]...[/ENT] tokens and the values in between
-                body_text = re.sub(r'\[ENT\].*?\[\/ENT\]', '', article['body'])
 
-                # Tokenize the body text using the word_tokenize function
-                tokens = word_tokenize(body_text)
+        for article in info['articles']:
+            # Remove the [ENT]...[/ENT] tokens and the values in between
+            body_text = re.sub(r'\[ENT\].*?\[\/ENT\]', '', article['body'])
 
-                # Join tokens back into a single string and split into sentences
-                body_text = ' '.join(tokens)
-                sentences = sent_tokenize(body_text)
+            # Tokenize the body text using the word_tokenize function
+            tokens = word_tokenize(body_text)
 
-                # Filter sentences to only include those that contain any of the specified entity names
-                relevant_sentences = [sent for sent in sentences if any(
-                    name in sent for name in entity_names)]
+            # Join tokens back into a single string and split into sentences
+            body_text = ' '.join(tokens)
+            sentences = sent_tokenize(body_text)
 
-                # Skip this article if no relevant sentences are found
-                if not relevant_sentences:
-                    continue
+            # Filter sentences to only include those that contain any of the specified entity names
+            relevant_sentences = [sent for sent in sentences if any(
+                name in sent for name in entity_names)]
 
-                # Check the recognized entities for each sentence
-                found_matching_entity = False  # Initialize a flag to false
-                for sentence in relevant_sentences:
-                    # Run NER on the sentence
+            # Skip this article if no relevant sentences are found
+            if not relevant_sentences:
+                continue
+
+            # Check the recognized entities for each sentence
+            found_matching_entity = False  # Initialize a flag to false
+            for sentence in relevant_sentences:
+                # Run NER on the sentence
+                with torch.no_grad():
                     entities = ner_pipeline(sentence)
-                    # Check if any of the entities match the specified entity names and are organizations
-                    for entity in entities:
-                        # Remove leading and trailing whitespace from the entity text
-                        entity_text = entity['word'].strip()
-                        if entity['entity_group'] == 'ORG' and entity_text in entity_names:
-                            filtered_articles_data[date]['articles'].append(
-                                article)
-                            filtered_articles_data[date]['number_of_articles'] += 1
-                            found_matching_entity = True  # Set the flag to true
-                            break  # Exit the inner loop as we've found a matching entity
-                    if found_matching_entity:
-                        break  # Exit the outer loop as we've found a matching entity
+                # Check if any of the entities match the specified entity names and are organizations
+                for entity in entities:
+                    # Remove leading and trailing whitespace from the entity text
+                    entity_text = entity['word'].strip()
+                    if entity['entity_group'] == 'ORG' and entity_text in entity_names:
+                        filtered_articles_data[date]['articles'].append(
+                            article)
+                        filtered_articles_data[date]['number_of_articles'] += 1
+                        found_matching_entity = True  # Set the flag to true
+                        break  # Exit the inner loop as we've found a matching entity
+                if found_matching_entity:
+                    break  # Exit the outer loop as we've found a matching entity
 
         end_time = time.time()  # Store the end time
         elapsed_time = end_time - start_time  # Calculate the elapsed time
